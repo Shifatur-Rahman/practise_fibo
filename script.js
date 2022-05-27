424a03b2e695f4a9be6a431ee475289c41b4c45d
Shifatur-Rahman <rjnovel28@gmail.com>
2022-05-27 18:36:36 +0600

    Practise fibonacci file

---
+++ script.js
@@ -0,0 +1,15 @@
+
+let t1=0,t2=1,t3=0, upperLimit;
+upperLimit = prompt('Enter your upper limit');
+console.log();
+
+while(t3<upperLimit){
+    
+t1=t2;
+t2=t3;
+
+document.write(t3+"\n\n" );
+t3=t1+t2;
+
+}
+
